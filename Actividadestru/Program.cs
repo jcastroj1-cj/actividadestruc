778d35c578d94ca79ea9644667cd4aa335e5311f
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-04-07 22:29:02 -0500

    Iniciando repositorio, se crea con record

---
+++ Actividadestru/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EstructuraApp
+{
+    // Definición del record struct
+    public record struct Estudiante(string Nombre, int Edad, double Promedio);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instancia
+
+            var alumno1 = new Estudiante("Carlos Pérez", 19, 8.5);
+            var alumno2 = new Estudiante("María López", 21, 9.2);
+            var alumno3 = new Estudiante("Juan Torres", 20, 7.8);
+
+
+        }
+    }
+}
